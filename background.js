dbc024f172ad3379eb1f57bfd322f2bc5b911393
KSABCarl <carl.holmberg@kunskapsgymnasiet.se>
2021-04-01 22:34:28 +0200

    Updated wiht fixes

--- background.js
+++ background.js
@@ -10,13 +10,13 @@ const clearCookies = (url) => {
   // you can add a conditional based on the url what cookies you want to remove, i.e.
   // > const cookieMatcher = urlInfo.origin.match(/domain.com/) ? /^ACookie/ : /./;
   // The regex /./ matches anything
-  const cookieMatcher = /./; 
-  chrome.cookies.getAll({ domain: urlInfo.host }, function (cookies) {
+  const cookieMatcher = /./;
+  chrome.cookies.getAll({ url: urlInfo.origin}, (cookies) => {
     cookies.forEach((c) => {
       if (c.name.match(cookieMatcher)) {
         chrome.cookies.remove({
           name: c.name,
-          url: url,
+          url: urlInfo.origin,
           storeId: c.storeId,
         });
       }
@@ -26,19 +26,16 @@ const clearCookies = (url) => {
 
 var requestMade = 0;
 
-// onHeadersReceived is before the actual page is returned which removes cookies from
-// the beginning. It may be enough with this but I've kept the original LetLogin-method
-// for cleaning up below.
-chrome.webRequest.onHeadersReceived.addListener(
-  (details) => {
-    clearCookies(details.url);
-  },
-  {
-    // Theses are the urls that the cleanup shold be triggerd on. They also need to be
-    // present in manifest.json
-    urls: ["*://www.youtube.com/*"], 
-  }
-);
+// chrome.webRequest.onHeadersReceived.addListener(
+//   (site) => {
+//     clearCookies(site.url);
+//   },
+//   {
+//     // Theses are the urls that the cleanup shold be triggerd on. They also need to be
+//     // present in manifest.json
+//     urls: ["*://www.youtube.com/*"], 
+//   }
+// );
 
 // This is LetLogins original method to handle cookie cleaning. I haven't actully checked
 // which cookies we need to remove and if the timer is actually neccessary...
@@ -55,3 +52,12 @@ chrome.webRequest.onBeforeRequest.addListener(
     urls: ["*://www.youtube.com/*"],
   }
 );
+
+// chrome.webRequest.onCompleted.addListener((site) => {
+//   clearCookies(site.url);
+// },
+// {
+//   // Theses are the urls that the cleanup shold be triggerd on. They also need to be
+//   // present in manifest.json
+//   urls: ["*://www.youtube.com/*"],
+// })
